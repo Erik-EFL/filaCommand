7e6cc366dfc2bd26692ad0b6557dc1e87c55ccd0
Erik-EFL <erik.lima.f@outlook.com>
2024-03-24 23:04:33 -0300

    feat: :bug: Fix roteamento

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ class App {
 
     this.app.get('/', (req: Request, res: Response) => res.json({ ok: true }));
 
-    this.app.use('/api/v1/user', route.users);
+    this.app.use('/user', route.users);
 
     this.app.use(errorMiddleware);
   }
